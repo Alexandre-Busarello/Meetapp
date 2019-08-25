5fcf8a2e6373e61ff0b2550b5c2ce9078f8197f7
Alexandre Busarello <busamar@LAPTOP-NKAJQ9SE.localdomain>
2019-08-25 09:07:37 -0300

    Create variabled for DB on env file

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 
 import express from 'express';
+import Youch from 'youch';
 import { resolve } from 'path';
 import cors from 'cors';
 import routes from './routes';
@@ -13,6 +14,8 @@ class App {
 
     this.middlewares();
     this.routes();
+
+    this.exceptionHandler();
   }
 
   middlewares() {
@@ -26,6 +29,17 @@ class App {
 
   routes() {
     this.server.use(routes);
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      if (process.env.NODE_ENV === 'development') {
+        const errors = await new Youch(err, req).toJSON();
+
+        return res.status(500).json(errors);
+      }
+      return res.status(500).json({ error: 'Internal Server Error' });
+    });
   }
 }
 
